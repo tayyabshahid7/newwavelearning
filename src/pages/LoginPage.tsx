56771effc65f2ae80b103b5446db119f2d3300cc
Carlitro <ccardozov@gmail.com>
2021-10-18 20:29:11 -0300

    Added @types/react-router-dom and simple routing structure

---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import { Typography } from "@mui/material";
+
+const LoginPage = () => {
+  return (
+    <Typography variant="h1">
+      the mui documentation.
+    </Typography>
+  );
+};
+
+export default LoginPage;
