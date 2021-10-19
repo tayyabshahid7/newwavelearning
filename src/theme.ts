8269e14de359c2b7ecbfa547ac69e52b6079f252
Carlitro <ccardozov@gmail.com>
2021-10-19 18:58:45 -0300

    Added custom colors and defaults theme props for Material-UI

--- src/theme.ts
+++ src/theme.ts
@@ -1,13 +1,58 @@
 import { createTheme, responsiveFontSizes } from "@mui/material/styles";
+declare module "@mui/material/styles" {
+  interface Palette {
+    orange: Palette["primary"];
+  }
+
+  // allow configuration using `createTheme`
+  interface PaletteOptions {
+    orange?: PaletteOptions["primary"];
+  }
+}
+
+// Update the Button's color prop options
+declare module "@mui/material/Button" {
+  interface ButtonPropsColorOverrides {
+    orange: true;
+  }
+}
 
 // A custom theme for nwl app
 let theme = createTheme({
   palette: {
     primary: {
-      main: "#0E4A66",
+      main: "#22B9D4",
+      light: "#0f0",
     },
     secondary: {
-      main: "#fd773b",
+      main: "#0E4A66",
+    },
+    orange: {
+      main: "#FD773B",
+    },
+  },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          color: "white",
+        },
+      },
+      defaultProps: {
+        disableElevation: true,
+      },
+    },
+    MuiTextField: {
+      defaultProps: {
+        InputLabelProps: {
+          shrink: true,
+        },
+      },
+      styleOverrides: {
+        root: {
+          backgroundColor: "white",
+        },
+      },
     },
   },
 });
