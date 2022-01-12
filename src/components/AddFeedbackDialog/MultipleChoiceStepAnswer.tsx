7d0119e4c9f6c29075b12b30e6efb288c2eb9bd9
Carlitro <ccardozov@gmail.com>
2022-01-12 18:39:34 -0300

    NW-66. Added multiple choice feedback dialog

--- src/components/AddFeedbackDialog/MultipleChoiceStepAnswer.tsx
+++ src/components/AddFeedbackDialog/MultipleChoiceStepAnswer.tsx
@@ -7,17 +7,15 @@ const MultipleChoiceStepAnswer = ({ stepAnswer }: any) => {
       <Typography>Question: {stepAnswer?.step.fields.question}</Typography>
       <Typography>Description: {stepAnswer?.step.fields.description}</Typography>
       <Typography> Answer:</Typography>
+      <Typography>Number of required answers: {stepAnswer?.step.fields.correct_answers}</Typography>
       <ul>
         {stepAnswer?.step.fields.answers.map((answer: any) => (
           <li>
             <Stack direction="row" spacing={3}>
               <Typography>{answer.text}</Typography>
-              <Typography color="green">{answer.correct && "Correct answer"}</Typography>
-              <Typography color="primary">
-                {stepAnswer.answer.includes(answer.id) && "- User Answer"}
-              </Typography>
-              <Typography color="error">
-                {!stepAnswer.answer.includes(answer.id) && "- User Answer"}
+              {answer.correct && <Typography color="green">Correct Answer</Typography>}
+              <Typography color={answer.correct ? "primary" : "error"}>
+                {stepAnswer.answer.includes(answer.id) && "Learner Answer"}
               </Typography>
             </Stack>
           </li>
