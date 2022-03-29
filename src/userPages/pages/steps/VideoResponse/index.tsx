19ed35bdc957018b7133fa4e94ab01c7e56ed004
Tayyab <tayyab.shahid7@gmail.com>
2022-03-29 14:30:41 +0500

    nw-107,106 done

---
+++ src/userPages/pages/steps/VideoResponse/index.tsx
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import { Grid } from "@mui/material";
+// @ts-ignore
+import { Recorder } from "react-voice-recorder";
+import "react-voice-recorder/dist/index.css";
+import "./style.scss";
+
+const AudioResponse = ({ audio }: any) => {
+  const [audioDetails, setAudioDetails] = useState({
+    url: null,
+    blob: null,
+    chunks: null,
+    duration: {
+      h: null,
+      m: null,
+      s: null,
+    },
+  });
+
+  const handleAudioStop = (data: any) => {
+    setAudioDetails(data);
+  };
+
+  const handleAudioUpload = (file: any) => {
+    console.log(file);
+  };
+
+  const handleReset = () => {
+    const reset = {
+      url: null,
+      blob: null,
+      chunks: null,
+      duration: {
+        h: null,
+        m: null,
+        s: null,
+      },
+    };
+    setAudioDetails(reset);
+  };
+
+  return (
+    <>
+      <Grid sx={{ display: "flex", justifyContent: "center", position: "relative" }}>
+        <Recorder
+          hideHeader={true}
+          record={false}
+          audioURL={audioDetails.url}
+          showUIAudio
+          handleAudioStop={(data: any) => handleAudioStop(data)}
+          handleAudioUpload={(data: any) => handleAudioUpload(data)}
+          handleReset={handleReset}
+        />
+      </Grid>
+    </>
+  );
+};
+
+export default AudioResponse;
