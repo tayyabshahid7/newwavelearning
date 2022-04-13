dd36ea2ead1bfa5acd269ed6140d1c2cab93c034
Tayyab <tayyab.shahid7@gmail.com>
2022-04-13 23:37:29 +0500

    nw-150 steps count on program page updated

--- src/services/common.ts
+++ src/services/common.ts
@@ -108,9 +108,6 @@ export const duplicateSection = async (sectionId: number | string) => {
 export const getProgrammeSections = async (programmeId: number, isAdmin: boolean) => {
   try {
     const response = await axs.get<SectionData[]>(`/programmes/${programmeId}/sections/`);
-    // {
-    //   params: { is_admin: isAdmin },
-    // }
     return response;
   } catch (error: any) {
     throw error;
