1ddeaf9ec68328883491b26bb5e1bbf4c4bd2245
Tayyab <tayyab.shahid7@gmail.com>
2022-05-31 18:46:35 +0500

    NW-185. Content step - increase image size issue fixed

--- src/userPages/pages/steps/TextContentQuestion/index.tsx
+++ src/userPages/pages/steps/TextContentQuestion/index.tsx
@@ -6,13 +6,7 @@ const TextContentQuestion = ({ image }: any) => {
   return (
     <>
       <Grid className="text-question text-image">
-        <img
-          style={{ padding: "3%" }}
-          src={image}
-          width="220px"
-          height="220px"
-          alt="New Wave Learning Logo"
-        />
+        <img src={image} alt="New Wave Learning Logo" />
       </Grid>
     </>
   );
