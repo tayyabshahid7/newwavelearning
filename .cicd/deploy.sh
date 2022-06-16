b9e170f879753de05139d9ac09d5908ccd747749
Fernando Cardozo <fernandojcardozov@gmail.com>
2022-06-16 01:25:57 -0400

    Add pipeline to deploy on every push to master

---
+++ .cicd/deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+string=`cat ~/ip`
+
+array=(${string//,/ })
+
+for i in "${!array[@]}"
+do
+    echo "Deploy project on server ${array[i]}"
+    ssh ubuntu@${array[i]} "
+        pm2 delete 0;\\
+        cd frontend &&\\
+        git fetch --all &&\\
+        git pull &&\\
+        rm -rf build &&\\
+        yarn install &&\\
+        yarn build &&\\
+        pm2 start ~/ecosystem.config.js"
+done
